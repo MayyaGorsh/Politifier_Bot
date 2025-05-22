1abb61c2e678bfd035c4b8fe86d20955f8b74568
Майя <mayyav.gorshenina@gmail.com>
2025-05-22 12:37:27 +0300

    Add corrector model to Politify_function.py

--- bot/Politify_function.py
+++ bot/Politify_function.py
@@ -1,4 +1,4 @@
-from transformers import T5ForConditionalGeneration, AutoTokenizer
+from transformers import T5ForConditionalGeneration, AutoTokenizer, pipeline
 import torch
 
 # Загружаем модель один раз при импорте
@@ -7,6 +7,8 @@ BASE_MODEL_NAME = "ai-forever/ruT5-base"
 
 tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL_NAME)
 model = T5ForConditionalGeneration.from_pretrained(MODEL_PATH)
+
+correction_pipeline = pipeline("text2text-generation", model="sdadas/byt5-text-correction")
 
 
 def Politify(initial_message, max_length=200):
@@ -18,4 +20,6 @@ def Politify(initial_message, max_length=200):
             num_return_sequences=1,  # один ответ
             do_sample=False  # модель всегда выдаёт наиболее вероятный вариант
         )
-    return tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    detoxed = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    corrected = correction_pipeline(f"<ru> {detoxed}", max_length=512)[0]['generated_text']
+    return corrected
